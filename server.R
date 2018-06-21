2513d2b7b9fd51c73d9f1e46de4b70ebd4dd5e64
JacintoCC <jacintocc0@gmail.com>
2018-06-21 19:58:07 +0200

    First version

---
+++ server.R
@@ -0,0 +1,232 @@
+library(shiny)
+library(ggplot2)
+library(reshape2)
+library(devtools)
+
+if("rNPBST" %in% rownames(installed.packages())){
+  library(rNPBST)
+} else {
+  devtools::install_github("JacintoCC/rNPBST")
+}
+
+format.table <- function(l){
+  names.l <- names(l)
+  max.length <- max(sapply(l, length))
+  which.vector <- sapply(l, length) > 1
+  table <- matrix("", ncol = max.length + 1, nrow = length(l) + sum(which.vector))
+  
+  for(i in 1:length(l)){
+    index.i <- i + sum(which.vector[1:(i-1)])
+    table[index.i, 1] <- names.l[i]
+    if(which.vector[i]){
+      table[index.i, seq(2, along.with = l[[i]])] <- names(l[[i]])
+      table[index.i + 1, seq(2, along.with = l[[i]])] <- l[[i]]
+    }
+    else{
+      table[index.i, 2] <- l[[i]]
+    }
+  }
+  colnames(table) <- c("Attribute", paste("Value", 1:max.length))
+  return(table)
+}
+
+apply.parametric.test <- function(df, test){
+  results <- switch (test,
+          "ANOVA" = {
+            long.df <- melt(df)
+            aov(value ~ var, long.df)
+          },
+          "t-test" = t.test(df[ ,1], df[ ,2])
+          )
+  return(results)
+}
+
+apply.non.parametric.test <- function(df, test){
+  results <- switch (test,
+          "Friedman" = rNPBST::friedman.test(df),
+          "FriedmanAR" = rNPBST::friedmanAR.test(df),
+          "Iman-Davenport" = rNPBST::imanDavenport.test(df),
+          "Quade" = quade.test(df))
+  return(results)
+}
+
+
+apply.bayesian.test <- function(df, test){
+  results <- switch (test,
+          "BayesFriedman" = rNPBST::bayesianFriedman.test(df, imprecise = T),
+          "Sign" = rNPBST::bayesianSign.test(df[ ,1], df[ ,2]),
+          "Signed-Rank" = rNPBST::bayesianSignedRank.test(df[ ,1], df[ ,2]),
+          "Corr-t-test" = rNPBST::bayesianCorrelatedT.test(df[ ,1], df[ ,2]))
+  return(results)
+}
+
+apply.test <- function(df, paradigm, test, ...){
+  switch (paradigm,
+          "Parametric" = {
+            results <- apply.parametric.test(df, test)
+          },
+          "Non-Parametric" = {
+            results <- apply.non.parametric.test(df, test)
+          },
+          "Bayesian" = {
+            results <- apply.bayesian.test(df, test)
+          }
+  )
+  return(results)
+}
+
+apply.test.two.datasets <- function(df1, df2, paradigm, test, ...){
+  results <- switch(paradigm,
+                    "Non-Parametric" = {
+                      switch(test,
+                             "GLRT-Multiple-Measures" = 
+                               rNPBST::multipleMeasuresGLRT(df1, df2))
+                    },
+                    "Bayesian" = {
+                      switch(test,
+                             "Bayesian-Multiple-Measures" = 
+                               rNPBST::bayesianMultipleConditions.test(df1, df2))
+                    })
+}
+
+# Define server logic to read selected file ----
+server <- function(input, output,session) {
+  # Read first dataset
+  df.reactive <- function(){
+    req(input$file1)
+    
+    df <- read.csv(input$file1$datapath,
+                   header = input$header,
+                   sep = input$sep,
+                   quote = input$quote)
+    return(df)
+  }
+  # Read additional dataset
+  df2.reactive <- function(){
+    if(input$additional){
+      req(input$file2)
+      
+      df2 <- read.csv(input$file2$datapath,
+                      header = input$header,
+                      sep = input$sep,
+                      quote = input$quote)
+      return(df2)
+    }
+  }
+  
+  # Show input table
+  output$contents <- renderDataTable(df.reactive()) 
+  output$contents.table2 <- renderDataTable(df2.reactive()) 
+  
+  # Update tests
+  observeEvent(input$additional,{
+    # Update available tests if two files
+    if(input$additional){
+      updateRadioButtons(session, inputId =  'checkboxParadigm', 
+                         label =  "Select Kind of test",
+                         choices = c("Non-Parametric", "Bayesian"),
+                         selected = "Non-Parametric")
+      updated.list <- switch(input$checkboxParadigm,
+                             "Non-Parametric" = list("GLRT-Multiples Measures" = "GLRT-Multiple-Measures"),
+                             "Bayesian" =  list("Multiple Measures" = "Bayesian-Multiple-Measures"))
+      updated.selected <- switch(input$checkboxParadigm,
+                                 "Non-Parametric" = "GLRT-Multiple-Measures",
+                                 "Bayesian" =  "Bayesian-Multiple-Measures")
+      updateSelectInput(session, inputId = 'test', label = "Test",  
+                        choices = updated.list, selected = updated.selected)
+    }
+    else{
+      updateRadioButtons(session, inputId =  'checkboxParadigm', 
+                         label =  "Select Kind of test",
+                         choices = c("Parametric","Non-Parametric", "Bayesian"),
+                         selected = "Parametric")
+      updated.list <- switch(input$checkboxParadigm,
+                             "Parametric" = list("ANOVA" = "ANOVA", "t-test" = "t-test"),
+                             "Non-Parametric" = {
+                               list("Friedman" = "Friedman", 
+                                    "Friedman Aligned-Rank" = "Friedman-AR",
+                                    "Iman-Davenport" = "Iman-Davenport", 
+                                    "Quade" = "Quade")
+                             },
+                             "Bayesian" = {
+                               list("Friedman" = "Friedman", "Sign test" = "Sign",
+                                    "Signed-rank" = "Signed-Rank",
+                                    "Correlated t-test" = "Corr-t-test")
+                             })
+      updated.selected <- switch(input$checkboxParadigm,
+                                 "Parametric" = "ANOVA",
+                                 "Non-Parametric" = "Friedman",
+                                 "Bayesian" = "BayesFriedman")
+      updateSelectInput(session, inputId =  'test', label = "Test",  
+                        choices = updated.list, selected = updated.selected)
+    }
+  })
+  
+  observeEvent(input$checkboxParadigm,{
+    # Update available tests if two files
+    if(input$additional){
+      updated.list <- switch(input$checkboxParadigm,
+                             "Non-Parametric" = list("GLRT-Multiples Measures" = "GLRT-Multiple-Measures"),
+                             "Bayesian" =  list("Multiple Measures" = "Bayesian-Multiple-Measures"))
+      updated.selected <- switch(input$checkboxParadigm,
+                                 "Non-Parametric" = "GLRT-Multiple-Measures",
+                                 "Bayesian" =  "Bayesian-Multiple-Measures")
+      updateSelectInput(session, inputId = 'test', label = "Test",  
+                        choices = updated.list, selected = updated.selected)
+    }
+    else{
+      updated.list <- switch(input$checkboxParadigm,
+                             "Parametric" = list("ANOVA" = "ANOVA", "t-test" = "t-test"),
+                             "Non-Parametric" = {
+                               list("Friedman" = "Friedman", 
+                                    "Friedman Aligned-Rank" = "Friedman-AR",
+                                    "Iman-Davenport" = "Iman-Davenport", 
+                                    "Quade" = "Quade")
+                             },
+                             "Bayesian" = {
+                               list("Friedman" = "BayesFriedman", "Sign test" = "Sign",
+                                    "Signed-rank" = "Signed-Rank",
+                                    "Correlated t-test" = "Corr-t-test")
+                             })
+      updated.selected <- switch(input$checkboxParadigm,
+                                 "Parametric" = "ANOVA",
+                                 "Non-Parametric" = "Friedman",
+                                 "Bayesian" = "BayesFriedman")
+      updateSelectInput(session, inputId =  'test', label = "Test",  
+                        choices = updated.list, selected = updated.selected)
+    }
+  })
+  
+  
+  # Compute Test
+  reactive.test <- reactive({
+    req(input$file1)
+    if(ncol(df.reactive() >= 2)){
+      if(input$additional)
+        test.result <- apply.test.two.datasets(df.reactive(), df2.reactive(), 
+                                               input$checkboxParadigm, 
+                                               input$test)
+      else{
+        test.result <- apply.test(df.reactive(), input$checkboxParadigm, input$test)
+      }
+    }
+    return(test.result)
+  })
+  
+  # Plot Test
+  reactive.plot <- reactive({
+    req(input$file1)
+    if(ncol(df.reactive() >= 2)){
+      if(!input$additional && input$checkboxParadigm == "Bayesian" && (input$test %in% c("Sign", "Signed-Rank", "Corr-t-test")))
+        plot <- switch(input$test, 
+                       "Corr-t-test" = rNPBST::plotPosterior(reactive.test(), names = c("A", "B"), dataset = "DF"),
+                       "Sign" = rNPBST::plotSimplex(reactive.test()$sample),
+                       "Signed-Rank" = rNPBST::plotSimplex(reactive.test()$sample))
+    }
+    return(plot)
+  })
+
+  output$tex.test.result <- renderText(rNPBST::htest2Tex(reactive.test()))
+  output$table.test.result <- renderDataTable(format.table(reactive.test()))
+  output$plot.test <- renderPlot(reactive.plot())
+}
---
+++ ui.R
@@ -0,0 +1,93 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  tags$head(
+    tags$style(HTML("hr {border-top: 1px solid #000000;}"))
+  ),
+  titlePanel(title = "Statistical Tests"),
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(      
+      fileInput("file1", "Choose main CSV file",
+                multiple = TRUE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+      # Input: Checkbox if file has header ----
+      checkboxInput("header", "Header", TRUE),
+      checkboxInput("rownames", "Rownames", FALSE),
+      # Input: Select separator ----
+      radioButtons("sep", "Separator",
+                   choices = c(Comma = ",",
+                               Semicolon = ";",
+                               Tab = "\t"),
+                   selected = ","),
+      hr(),
+      checkboxInput("additional", "Multiple files", FALSE),
+      conditionalPanel(
+        condition = "input.additional",
+        fileInput("file2", "Choose additional CSV file",
+                  multiple = TRUE,
+                  accept = c("text/csv",
+                             "text/comma-separated-values,text/plain",
+                             ".csv")),
+        # Input: Checkbox if file has header ----
+        checkboxInput("header2", "Header", TRUE),
+        checkboxInput("rownames2", "Rownames", FALSE),
+        # Input: Select separator ----
+        radioButtons("sep2", "Separator",
+                     choices = c(Comma = ",",
+                                 Semicolon = ";",
+                                 Tab = "\t"),
+                     selected = ",")
+      )
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      # Output: Histogram ----
+      tabsetPanel(type = "tabs",
+                  tabPanel("Show input dataset", 
+                           h3("Dataset"),
+                           column(12, dataTableOutput('contents')),
+                           conditionalPanel(
+                             condition = "input.additional",
+                             hr(),
+                             h3("Additional dataset"),
+                             column(12, dataTableOutput('contents.table2'))
+                           )),
+                  tabPanel("Test results",
+                           radioButtons("checkboxParadigm", "Select Kind of test",
+                                        choices = c("Parametric", "Non-Parametric", "Bayesian"),
+                                        selected = "Parametric"),
+                           fluidRow(selectInput('test', "Test", 
+                                                choices = list("None" = 0)),
+                                    conditionalPanel("(!input.additional) &&  (input.checkboxParadigm == \"Bayesian\")",
+                                                     checkboxInput("checkboxPlot", "Plot", FALSE))
+                           ),
+                           hr(), 
+                           h4("Table Output"),
+                           dataTableOutput("table.test.result"),
+                           hr(),
+                           h4("Latex Output"),
+                           verbatimTextOutput("tex.test.result"),
+                           conditionalPanel("(!input.additional) &&  (input.checkboxParadigm == \"Bayesian\") && (input.checkboxPlot)",
+                                            plotOutput("plot.test"))
+                           )
+    ))
+))
+
+
+
+
